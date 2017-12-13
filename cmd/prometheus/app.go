75369f68a8365c9100b67daab694a3af5c898792
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-12-12 23:13:07 -0700

    Adds custom metric to sample prometheus app
    - This provides an example of a +Inf value in prometheus which fails to directly json.Marshal

--- cmd/prometheus/app.go
+++ cmd/prometheus/app.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	normDomain            = flag.Float64("normal.domain", 0.0002, "The domain for the normal distribution.")
+	normMean              = flag.Float64("normal.mean", 0.00001, "The mean for the normal distribution.")
+	rpcDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "rpc_durations_histogram_seconds",
+		Help:    "RPC latency distributions.",
+		Buckets: prometheus.LinearBuckets(*normMean-5**normDomain, .5**normDomain, 20),
+	})
+)
+
+func init() {
+	prometheus.MustRegister(rpcDurationsHistogram)
+}
 
 func main() {
 	port := os.Getenv("PORT")
